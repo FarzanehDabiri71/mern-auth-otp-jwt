89d3281ec77b20c472f89298c0e5d9553ed72aa5
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2025-03-13 15:12:57 +0330

    Added routes setup in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import all components
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Username from "./components/Username";
 // root routes
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Root Route</div>,
+    element: <Username />,
   },
   {
     path: "/register",
-    element: <div>Register Route</div>,
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "/pageNotFound",
+    element: <PageNotFound />,
   },
 ]);
 function App() {
